b3753562ebf14755a1dc3e74f47ef4df51920bc0
Joseph Chee Chang <josephcc@cmu.edu>
2024-10-28 17:30:47 -0700

    Track past queries with cookies

--- ui/src/pages/Home.tsx
+++ ui/src/pages/Home.tsx
@@ -4,12 +4,19 @@ import { useNavigate, useParams } from "react-router-dom";
 import { CircularProgress } from '@mui/material';
 import { Results } from '../components/Results';
 import { createTask } from '../api/utils';
+import { useCookies } from 'react-cookie';
 
 
 export const Home = () => {
 
   const navigate = useNavigate();
   const { taskId } = useParams();
+
+  const [cookies, setCookie] = useCookies(['history']);
+  if (!cookies.history) {
+    setCookie('history', {}, { path: '/' });
+  }
+
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
   const handleSubmit = useCallback(async (query: string) => {
@@ -21,9 +28,13 @@ export const Home = () => {
     console.log(newStatus, newStatus.task_id);
     if (newStatus.task_id) {
       navigate(`/query/${newStatus.task_id}`, { replace: true });
+      if (!cookies.history[newStatus.task_id]) {
+        setCookie('history', { ...cookies.history, [newStatus.task_id]: {
+          query, taskId: newStatus.task_id, timestamp: Date.now()
+        }}, { path: '/' });
+      }
     }
   }, []);
-
 
   return (
     <div style={{ paddingLeft: '25px' }}>
