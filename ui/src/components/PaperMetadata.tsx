df994316199eece5a27620068f2f588d2500ac1c
Joseph Chee Chang <josephcc@cmu.edu>
2024-11-05 09:57:54 -0800

    Render paper metadata

---
+++ ui/src/components/PaperMetadata.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Card, CardContent, CardMedia, CircularProgress, Typography } from '@mui/material';
+
+import TimeAgo from 'javascript-time-ago'
+
+import en from 'javascript-time-ago/locale/en'
+TimeAgo.addDefaultLocale(en)
+
+import { useTimeAgo } from 'react-time-ago'
+import { AuthorType } from 'src/api/utils';
+
+
+
+export interface PropType {
+  authors: AuthorType[];
+  year: number;
+  venue: string;
+  maxAuthors?: number;
+}
+
+export const PaperMetadata: React.FC<PropType> = (props) => {
+  const { authors: allAuthors, year, venue, maxAuthors = 6} = props;
+  const authors = allAuthors.slice(0, maxAuthors);
+  const truncated = allAuthors.length > maxAuthors;
+
+  return (
+    <div style={{ display: 'inline-block' }}>
+      {authors.map((author, index) => (
+        <>
+          <a key={author.authorId} href={`https://www.semanticscholar.org/author/${author.authorId}`}>{author.name}</a>
+          {index < authors.length - 1 ? ', ' : ''}
+        </>
+      ))}{truncated ? 'et al' : ''}. ${year}. {venue}.
+    </div>
+  );
+};
