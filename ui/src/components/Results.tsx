f57e0c2410595a7ef0683c48a4b8f5596989d8c7
Joseph Chee Chang <josephcc@cmu.edu>
2024-11-12 01:12:15 -0800

    Fix loading progress

--- ui/src/components/Results.tsx
+++ ui/src/components/Results.tsx
@@ -1,12 +1,10 @@
-import React, { useCallback, useEffect } from 'react';
-import { Box, Button, ButtonGroup, LinearProgress } from '@mui/material';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Button, ButtonGroup } from '@mui/material';
 import { StatusType, updateStatus } from '../api/utils';
-import { Report } from './report/Report';
 import { useQueryHistory } from './shared';
 import { useNavigate } from 'react-router-dom';
 import { Progress, ProgressPropType } from './Progress';
 import { Sections } from './Sections';
-
 
 const DEFAULT_INTERVAL = 3000;
 
@@ -20,8 +18,14 @@ export const Results: React.FC<PropType> = (props) => {
   const { history, setHistory } = useQueryHistory();
   const navigate = useNavigate();
 
-  const [status, setStatus] = React.useState<StatusType | undefined>();
-  const [_isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [status, setStatus] = useState<StatusType | undefined>();
+
+  const [progressProps, setProgressProps] = useState<Omit<ProgressPropType, 'isRunning'>>({
+    estimatedTime: 'Loading...',
+    startTime: -1,
+    status: 'Loading...',
+    httpStatus: 200
+  })
 
   const handleDeleteTask = useCallback(() => {
     if (confirm('Are you sure you want to delete this? This cannot be undone.')) {
@@ -38,59 +42,50 @@ export const Results: React.FC<PropType> = (props) => {
 
   useEffect(() => {
     const timeoutIds: number[] = [];
+
     const inner = async () => {
-      setIsLoading(true);
       const newStatus = await updateStatus(taskId);
-      setIsLoading(false);
       console.log('setStatus', newStatus)
       setStatus(newStatus);
-      const taskRunning = !newStatus || 'task_status' in newStatus
-      if (taskRunning && newStatus.httpStatus !== 404) {
-        const timeoutId = window.setTimeout(inner, interval);
-        timeoutIds.push(timeoutId);
+
+      if (newStatus?.httpStatus !== 200) {
+        console.log('A')
+        setProgressProps({
+          estimatedTime: 'Error',
+          startTime: -1,
+          status: newStatus?.detail ?? 'Something went wrong - please try asking again',
+          httpStatus: newStatus?.httpStatus ?? 500
+        })
+      } else {
+        try {
+          console.log('B')
+          const startTime = parseFloat(newStatus?.task_status.split(':').at(0) ?? '0')
+          const statusText = newStatus?.task_status.split(':').at(-1) ?? 'Loading...'
+          setProgressProps({
+            estimatedTime: newStatus?.estimated_time?.split(':')?.at(-1) ?? 'Loading...',
+            startTime,
+            status: statusText,
+            httpStatus: newStatus?.httpStatus ?? 200
+          })
+        } catch (e) {
+          console.error('error parsing status', e);
+        }
+        const taskRunning = 'task_status' in (newStatus ?? {})
+        if (taskRunning) {
+          const timeoutId = window.setTimeout(inner, interval);
+          timeoutIds.push(timeoutId);
+        }
       }
     }
     inner();
     return () => {
       timeoutIds.forEach(clearTimeout);
     }
-  }, [taskId, interval, setIsLoading]);
+  }, [taskId, interval]);
 
+  const taskRunning = 'task_status' in (status ?? {})
   // const section = status?.task_result?.sections.at(-1);
   const sections = status?.task_result?.sections ?? [];
-  let progressProps: Omit<ProgressPropType, 'isRunning'> = {
-    estimatedTime: 'Loading...',
-    startTime: -1,
-    status: 'Loading...',
-    httpStatus: 200
-  }
-  if (status?.httpStatus !== 200) {
-    console.log('A')
-    progressProps = {
-      estimatedTime: 'Error',
-      startTime: -1,
-      status: status?.detail ?? 'Something went wrong - please try asking again',
-      httpStatus: status?.httpStatus ?? 500
-    }
-  }
-  // if (!status?.task_result) {
-  const taskRunning = !status || 'task_status' in status
-  console.log('taskRunning', taskRunning, status)
-  if (taskRunning && status?.httpStatus !== 200) {
-    try {
-      console.log('B')
-      const startTime = parseFloat(status?.task_status.split(':').at(0) ?? '0')
-      const statusText = status?.task_status.split(':').at(-1) ?? 'Loading...'
-      progressProps = {
-        estimatedTime: status?.estimated_time?.split(':')?.at(-1) ?? 'Loading...',
-        startTime,
-        status: statusText,
-        httpStatus: status?.httpStatus ?? 200
-      }
-    } catch (e) {
-      console.error('error parsing status', e);
-    }
-  }
   const handleScrollToDisclaimer = useCallback(() => {
     document.querySelector('.disclaimer')?.scrollIntoView({ behavior: 'smooth' });
   }, []);
@@ -109,35 +104,9 @@ export const Results: React.FC<PropType> = (props) => {
           <Button color='secondary' onClick={handleDeleteTask}>Remove from history</Button>
         </ButtonGroup>
       </div>
-      <Progress {...progressProps} isRunning={taskRunning} />
+      {(taskRunning || status?.httpStatus !== 200) && <Progress {...progressProps} isRunning={taskRunning} />}
       {sections.length > 0 && (
-        <>
-          {/* <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center',
-              width: '100%',
-              justifyContent: 'space-between',
-              marginBottom: '6px',
-              '& > *': {
-                m: 0,
-              },
-            }}
-          >
-            {!(taskRunning) && (
-              <>
-                <ButtonGroup size="small" aria-label="Small button group" style={{ marginRight: '12px' }}>
-                  <Button key="one">Good</Button>
-                  <Button key="two">Bad</Button>
-                </ButtonGroup>
-                <Button key="three" onClick={handleDeleteTask}>Remove from history</Button>
-              </>
-            )}
-          </Box> */}
-
-          <Sections sections={sections} />
-        </>
+        <Sections sections={sections} />
       )}
     </div>
   );
