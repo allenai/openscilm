aaf10072db636812fd46aff575fc8d67d024ba02
Joseph Chee Chang <josephcc@cmu.edu>
2024-11-14 11:41:54 -0800

    Also link papers to s2 from reference

--- ui/src/components/PaperMetadataString.tsx
+++ ui/src/components/PaperMetadataString.tsx
@@ -8,10 +8,11 @@ export interface PropType {
   venue: string;
   maxAuthors?: number;
   title: string;
+  corpusId: number;
 }
 
 export const PaperMetadataString = (props: PropType): string => {
-  const { authors: allAuthors, title, year, venue, maxAuthors = 6} = props;
+  const { authors: allAuthors, title, year, venue, maxAuthors = 6, corpusId } = props;
   const authors = allAuthors.slice(0, maxAuthors);
   const truncated = allAuthors.length > maxAuthors;
 
@@ -19,7 +20,7 @@ export const PaperMetadataString = (props: PropType): string => {
       `${authors.map((author, index) => {
         const out = (`
           <a key="${author.authorId}" target='_blank' href="https://www.semanticscholar.org/author/${author.authorId}">${author.name}</a>${index < authors.length - 1 ? ', ' : ''}`)
-        return out}).join('')}${truncated ? 'et al' : ''}. ${title}. ${year}. ${venue}.
+        return out}).join('')}${truncated ? ' et al' : ''}. <a target='_blank' href="https://www.semanticscholar.org/p/${corpusId}">${title}</a>. ${year}. ${venue}.
       `
   );
 };
